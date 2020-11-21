5be9a3f3eb13f1a4735f626424b080764520a826
Abbe-Al-shammari <abbas1995abbe@gmail.com>
2020-11-21 08:33:46 +0300

    first

---
+++ MLConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MLConsoleML.Model;
+
+namespace MLConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+            input.SentimentText = "work at enjaz";
+            ModelOutput modelOutput = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text:{input.SentimentText}\nIs Toxic:{modelOutput.Prediction}");
+            Console.ReadKey();
+        }
+    }
+}
